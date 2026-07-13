bf4a529c4cc9e59c4a5caba6f6053ba7e237aab1
Amarnadh2006 <kasiamarnadh06@gmail.com>
2026-07-13 23:03:31 +0530

    Time: 17 ms (54.86%) | Memory: 12.5 MB (91.91%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.py
+++ 125-valid-palindrome/valid-palindrome.py
@@ -1,13 +1,14 @@
 class Solution(object):
     def isPalindrome(self, s):
-        r = s.lower()
-        for ch in r:
-            if (97 <= ord(ch) <= 122) or (48 <= ord(ch) <= 57):
-                continue
-            else:
-                r = r.replace(ch, "", 1)
-        p = r[::-1]
-        if r == p:
-            return True
-        else:
-            return False
+        l = 0
+        r = len(s) - 1
+        while l < r:
+            while l < r and not s[l].isalnum():
+                l += 1
+            while l < r and not s[r].isalnum():
+                r -= 1
+            if s[l].lower() != s[r].lower():
+                return False
+            l += 1
+            r -= 1
+        return True

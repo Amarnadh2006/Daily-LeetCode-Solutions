1415d8d0379d4f1a13fbf42422a8185de3633b63
Kasi Amarnadh <kasiamarnadh06@gmail.com>
2026-09-23 22:56:23 +0530

    Time: 11 ms (97.75%) | Memory: 22 MB (35.14%) - LeetSync

--- 455-assign-cookies/assign-cookies.py
+++ 455-assign-cookies/assign-cookies.py
@@ -3,7 +3,9 @@ class Solution:
         g.sort()
         s.sort()
         i,j,c=0,0,0
-        while (i!=len(g) and j!=len(s)):
+        p = len(g)
+        q = len(s)
+        while (i!=p and j!=q):
             if g[i]<=s[j]:
                 c+=1
                 i+=1
